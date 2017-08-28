e80694656431196b28dd9aebf9af0944518d4487
COPA-DATA Product Management <27494503+CopaDataPM@users.noreply.github.com>
2017-08-28 18:38:36 +0200

    OPCUA32API HowTo

---
+++ DriverConfigurationSamples/DriverCommon/Log.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using AddInSampleLibrary.Logging;
+using NLog;
+using Scada.AddIn.Contracts;
+
+namespace DriverCommon
+{
+    public class Log
+    {
+        private readonly IEditorApplication _editorApplication;
+        private readonly string _driverApiName;
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public Log(IEditorApplication editorApplication, string driverIdent)
+        {
+            var configurator = new NLogConfigurator();
+            configurator.Configure();
+
+            _editorApplication = editorApplication;
+            _driverApiName = driverIdent + "API";
+        }
+
+        public void Message(string msgText)
+        {
+            string text = $" - [{_driverApiName}]: {msgText}";
+            _editorApplication.DebugPrint(text, DebugPrintStyle.Standard);
+            Logger.Info(text);
+        }
+
+        public void InvalidPropertyValueMessage(string propName)
+        {
+            string text = $" - [{_driverApiName}]:     [{propName}] is invalid (and remains invalid)";
+            _editorApplication.DebugPrint(text, DebugPrintStyle.Standard);
+            Logger.Warn(text);
+        }
+
+        public void ExpectionMessage(string msgText, Exception ex)
+        {
+            string text = $" - [{_driverApiName}]:     [{msgText}] (Exception: {ex.Message})";
+            _editorApplication.DebugPrint(text, DebugPrintStyle.Standard);
+            Logger.Error(ex, text);
+        }
+
+        public void FunctionEntryMessage(string msgText)
+        {
+            _editorApplication.DebugPrint($" - [{_driverApiName}]:   {msgText}", DebugPrintStyle.Standard);
+        }
+        public void FunctionExitMessage()
+        {
+        }
+        public void PropertyModifiedMessage(string propName, object orgValue, object newValue, string propType)
+        {
+            _editorApplication.DebugPrint($" - [{_driverApiName}]:    [{propName}] from [{orgValue}] to [{newValue}] (value type: {propType})", DebugPrintStyle.Standard);
+        }
+    }
+}
---
+++ DriverConfigurationSamples/OPCUA32API/EditorWizardExtension.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using DriverCommon;
+using Scada.AddIn.Contracts;
+
+namespace OPCUA32_API
+{
+    /// <summary>
+    /// Description of Editor Wizard Extension.
+    /// </summary>
+    [AddInExtension("OPCUA32_API", "DriverContext OPCUA32 API, Import and Export", "Drivers API/Export/Import")]
+    public class EditorWizardExtension : IEditorWizardExtension
+    {
+        private Log _log;
+        private DriverContext _driverContext;
+
+        const string DriverIdent = "OPCUA32";
+        const string DriverName = "OPC UA Client Treiber";
+        const string SuffixBefore = "before";
+        const string XmlSuffixAfter = "after";
+
+        #region IEditorWizardExtension implementation
+
+        public void Run(IEditorApplication context, IBehavior behavior)
+        {
+            _log = new Log(context, DriverIdent);
+
+            try
+            {
+                _driverContext = new DriverContext(context, _log, DriverName, false);
+
+                // enter your code which should be executed when starting the SCADA Editor Wizard
+
+                _log.Message("begin test");
+
+                _driverContext.Export(SuffixBefore);
+
+                if (_driverContext.OpenDriver(10))
+                {
+                    _driverContext.ModifyCommonProperties();
+                    _driverContext.ModifyComProperties();
+
+                    ModifyServers();
+
+                    _driverContext.CloseDriver();
+
+                    _driverContext.Export(XmlSuffixAfter);
+                    _driverContext.Import(SuffixBefore);
+                }
+
+                _log.Message("end test");
+            }
+            catch (Exception ex)
+            {
+                _log.ExpectionMessage($"An exception has been thrown: {ex.Message}", ex);
+                throw;
+            }
+        }
+
+        private void ModifyServers()
+        {
+            _log.Message("modify servers");
+
+            string[] propItems;
+            uint srvrCount;
+            _driverContext.GetNodeInfo("DrvConfig.Server", out propItems, out srvrCount);
+
+            for (uint idxI = 0; idxI < srvrCount; idxI++)
+            {
+                ModifyServer(idxI);
+            }
+        }
+
+        private void ModifyServer(uint srvrIndex)
+        {
+            var srvrNamePrefix = "DrvConfig.Server[" + srvrIndex + "].";
+
+            srvrIndex = srvrIndex + 1;
+
+            _log.Message($"modify {srvrIndex}. server");
+
+            _driverContext.SetUnsignedProperty(srvrNamePrefix + "NetAddress", srvrIndex, 0, 65535, true);
+            _driverContext.SetBooleanProperty(srvrNamePrefix + "StorePassword");
+            _driverContext.SetStringProperty(srvrNamePrefix + "Username", "555", true);
+            _driverContext.SetStringProperty(srvrNamePrefix + "Password", "666", true);
+            _driverContext.IncreaseUnsignedProperty(srvrNamePrefix + "SecurityMode", 1, 3);
+
+            ModifyServerCommunication(srvrNamePrefix);
+            ModifyServerProtocol(srvrNamePrefix);
+        }
+
+        private void ModifyServerCommunication(string csPathname)
+        {
+            var commNamePrefix = csPathname + "Communication.";
+
+            _log.Message("modify communication");
+
+            _driverContext.IncreaseDoubleProperty(commNamePrefix + "PublishInterval", -100000.0, 100000.0);
+            _driverContext.IncreaseUnsignedProperty(commNamePrefix + "LifeTimeCount", 1, 100000);
+            _driverContext.IncreaseUnsignedProperty(commNamePrefix + "KeepAliveCount", 1, 1000);
+            _driverContext.IncreaseDoubleProperty(commNamePrefix + "SamplingInterval", -100000.0, 100000.0);
+        }
+
+        private void ModifyServerProtocol(string csPathname)
+        {
+            var protNamePrefix = csPathname + "Protocol.";
+
+            _log.Message("modify protocol");
+
+            _driverContext.IncreaseUnsignedProperty(protNamePrefix + "DataChangeFilter", 0, 1);
+            _driverContext.SetBooleanProperty(protNamePrefix + "AbsoluteDeadband");
+            _driverContext.SetBooleanProperty(protNamePrefix + "PersistentNodeIDs");
+            _driverContext.SetBooleanProperty(protNamePrefix + "ReadInitialValues");
+            _driverContext.SetBooleanProperty(protNamePrefix + "ArraysOnIndexBasis");
+        }
+
+
+        #endregion
+    }
+
+}
