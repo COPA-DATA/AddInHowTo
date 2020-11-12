5b6e6a2de5b1997158dfe652330cc78beaa18d23
Christian Feitler <Christian.Feitler@copadata.com>
2020-11-12 16:52:47 +0100

    As requested by subsidiaries and customers here is an example on how to create an XML file which is then imported to zenon

---
+++ ObjectCreationWithXmlImport/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlImporter
+{
+  public static class Constants
+  {
+    public const string Apartment = "Apartment";
+    public const string SystemModel = "SystemModel";
+    public const string Model = "Model";
+    public const string Groups = "Groups";
+    public const string Group = "Group";
+    public const string ChildGroups = "ChildGroups";
+    public const string Name = "Name";
+    public const string Guid = "GUID";
+  }
+}
