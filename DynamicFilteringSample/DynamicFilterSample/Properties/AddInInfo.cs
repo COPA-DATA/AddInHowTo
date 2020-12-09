77ed42e6750f49f0bb1233b2af321b6b477e31ca
CFeitler <c.feitler@gmx.at>
2020-12-09 16:52:26 +0100

    Added Sample for setting dynamic Properties of a function before execution.

---
+++ DynamicFilteringSample/DynamicFilterSample/Properties/AddInInfo.cs
@@ -0,0 +1,10 @@
+﻿using Mono.Addins;
+
+// Declares that this assembly is an add-in
+[assembly: Addin("DynamicFilterSample", "1.0",Namespace = "com.copadata.training")]
+
+// Declares that this add-in depends on the scada v1.0 add-in root
+[assembly: AddinDependency("::scada", "1.0")]
+
+[assembly: AddinName("DynamicFilterSample")]
+[assembly: AddinDescription("This wizard is for demonstration purposes only. It shows the usage of dynamic properties in a project wizard extention.")]
